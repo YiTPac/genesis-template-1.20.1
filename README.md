cf6740c3f51f8dbc1f1152879fb3640240159f11
YiTPac_ <128908804+YiTPac@users.noreply.github.com>
2026-06-15 01:19:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# genesis-template-1.20.1
+A Minecraft mod
---
+++ src/main/java/com/prism/genesis/GenesisClient.java
@@ -0,0 +1,10 @@
+package com.prism.genesis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GenesisClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
